83b363ab32c1f621560e9283bef173900d0aa874
sasha <sasha.mikayelyan@optimumparthner.co>
2024-02-05 13:16:20 +0400

    add:modules for home and app component

--- angular-fe/src/app/components/feature/home/home.component.ts
+++ angular-fe/src/app/components/feature/home/home.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { TodoListComponent } from '../../shared/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [TodoListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
